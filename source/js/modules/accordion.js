3df3bb74157ba2d7ba354e6de486c42628e55050
SakharovaKsu <nagis38@gmail.com>
2023-03-02 00:17:39 +0500

    Исправление ошибок

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,34 +1,17 @@
 export const initAccordion = () => {
   const accordions = document.querySelectorAll('.accordion');
 
-	accordions.forEach(el => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
+  accordions.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      const self = e.target.closest('.accordion');
 
-			if(self.classList.contains('accordion__content--open')) {
-				self.classList.remove('accordion__content--open');
-			} else {
-				accordions.forEach(el => {
-					el.classList.toggle('accordion__content--open');
-				})
-			} 
-			
-			// accordions.forEach(el => {
-			// 	el.classList.remove('accordion__open');
-
-				// if (self.classList.contains('accordion__open')) {
-				// 	control.setAttribute('aria-expanded', true);
-				// 	content.setAttribute('aria-hidden', false);
-				// } else {
-				// 	control.setAttribute('aria-expanded', false);
-				// 	content.setAttribute('aria-hidden', true);
-				// }
-
-				
-			// })
-			
-			self.classList.add('accordion__open');
-
-		});
-	});
-}
+      accordions.forEach((el) => {
+        if (el === self && !self.classList.contains('accordion__content--open')) {
+          el.classList.add('accordion__content--open');
+        } else {
+          el.classList.remove('accordion__content--open');
+        }
+      });
+    });
+  });
+};
