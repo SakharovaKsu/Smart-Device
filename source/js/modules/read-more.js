72eb41569bfd7a1e81bd9a924e94732d62901819
SakharovaKsu <nagis38@gmail.com>
2023-03-02 18:20:04 +0500

    Добавила текст для информационного блока

--- source/js/modules/read-more.js
+++ source/js/modules/read-more.js
@@ -1,17 +1,17 @@
 export const readMore = () => {
-  const dots = document.getElementById('dots');
-  const more = document.querySelector('.information__text--more');
+  // const dots = document.getElementById('dots');
+  const more = document.querySelectorAll('.information__text--more');
   const button = document.querySelector('.information__button');
 
-  button.addEventListener('click', (e) => {
-    if(dots.style.display === 'none') {
-      dots.style.display = 'block';
-      button.innerHTML = 'Подробнее';
-      more.style.display = 'none';
-    } else {
-      dots.style.display = 'none';
-      button.innerHTML = 'Скрыть';
-      more.style.display = 'block';
+  button.addEventListener('click', () => {
+    for (let i = 0; i < more.length; i++) {
+      if(more[i].style.display === 'none') {
+        button.innerHTML = 'Скрыть';
+        more[i].style.display = 'block';
+      } else {
+        button.innerHTML = 'Подробнее';
+        more[i].style.display = 'none';
+      }
     }
   })
 };
